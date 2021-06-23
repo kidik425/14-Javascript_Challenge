3794ea0207b1362d9bd867d49bed712a3db3385f
Steve Walker <bsteven.walker@gmail.com>
2021-06-23 13:35:14 -0400

    added all parameters for option 2

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,43 +1,33 @@
+//Went fancy with the config
+var dropdownConfig = config;
 var tableData = data;
 
-// Get a reference to the table body
+var dIDs = [];
+
+//Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Create initial data table
-data.forEach((sighting) => {
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
+renewTable();
 
-//Create Drop Down References 
-var dropdownDate = document.getElementById("selectDate");
-var dropdownCity = document.getElementById("selectCity");
-var dropdownState = document.getElementById("selectState");
-var dropdownCountry = document.getElementById("selectCountry");
-var dropdownShape = document.getElementById("selectShape");
-
-//Return list of values in array
-// const dateList = [...new Set( data.map(obj => obj.datetime)) ];
-const dateList = returnList('datetime',false);
-const cityList = returnList('city', true);
-const stateList = returnList('state', true);
-const countryList = returnList('country', false);
-const shapeList = returnList('shape', true);
-
-//console.log(shapeList); //sanity checks
-
-//Call methods to populate each dropdown
-getValues(dropdownDate, dateList);
-getValues(dropdownCity, cityList);
-getValues(dropdownState, stateList);
-getValues(dropdownCountry, countryList);
-getValues(dropdownShape, shapeList);
+//dynamically create each dropdown based on config file
+config.forEach((cfig) => {
+    //console.log (cfig) //sanity check
+    var dropdown = document.getElementById(cfig.htmlTag);
+    const list = returnList(cfig.name,cfig.sorted);
+    getValues(dropdown, list);
+    dIDs.push(cfig.htmlTag);
+});
+//console.log(dIDs); //sanity check
 
 //Build out filter section
+// Select the drop down
+var filters = d3.selectAll("select");
+var refresh = d3.selectAll("button");
+//console.log(filters);
 
+//Create event handlers 
+filters.on("change", runFilter);
+refresh.on("click", deafultFilters)
 
 /////////function section
 //Returns a list of values either sorted or not depending on parameters
@@ -58,24 +48,25 @@ for(var i = 0; i < list.length; i++){
     el.value = newOption;
 
     dropdown.append(el);
-}
-};
+}};
 
-// Complete the event handler function for the form
+//Complete the event handler function for the form
 function runFilter() {
-
-    // Prevent the page from refreshing
+    //Prevent the page from refreshing
     d3.event.preventDefault();
 
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("tbody");
-
+    //Get selected value
+    var dropdownKey = d3.select(this).property("id");
+    var dropdownValue = d3.select(this).property("value");
+    
+    //Get field name from json based on Key
+    var filteredConfig = dropdownConfig.filter(rec => rec["htmlTag"] === dropdownKey);
+    var key = Object.keys(filteredConfig)[0];
+    var field = filteredConfig[key]['name'];
+    
     // Get the value property of the input element
-    var inputValue = inputElement.property("value");
-
-    var filteredData = tableData.filter(rec => rec.datetime === inputValue);
-
-    // console.log(filteredData); //sanity check
+    var filteredData = tableData.filter(rec => rec[field] === dropdownValue);
+    //console.log(filteredData); //sanity check
 
     var tbody = d3.select("tbody");
 
@@ -90,37 +81,19 @@ function runFilter() {
       });
 }
 
-// // Select the button
-// var button = d3.select("#filter-btn");
+//
+function deafultFilters(){
+    window.location.reload();
+    return false();
+}
 
-// // Create event handlers 
-// button.on("click", runFilter);
-
-// // Complete the event handler function for the form
-// function runFilter() {
-
-//     // Prevent the page from refreshing
-//     d3.event.preventDefault();
-
-//     // Select the input element and get the raw HTML node
-//     var inputElement = d3.select("#datetime");
-
-//     // Get the value property of the input element
-//     var inputValue = inputElement.property("value");
-
-//     var filteredData = tableData.filter(rec => rec.datetime === inputValue);
-
-//     // console.log(filteredData); //sanity check
-
-//     var tbody = d3.select("tbody");
-
-//     tbody.html("");
-
-//     filteredData.forEach((sighting) => {
-//         var row = tbody.append("tr");
-//         Object.entries(sighting).forEach(([key, value]) => {
-//           var cell = row.append("td");
-//           cell.text(value);
-//         });
-//       });
-// }
+//Create initial data table
+function renewTable() {
+    data.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });
+}
---
+++ UFO-Level-1/static/js/config.js
@@ -0,0 +1,26 @@
+var config = [{
+  name: "datetime",
+  htmlTag: "selectDate",
+  sorted: false
+ },
+ {
+  name: "city",
+  htmlTag: "selectCity",
+  sorted: true
+ },
+ {
+  name: "state",
+  htmlTag: "selectState",
+  sorted: true
+ },
+ {
+  name: "country",
+  htmlTag: "selectCountry",
+  sorted: false
+ },
+ {
+  name: "shape",
+  htmlTag: "selectShape",
+  sorted: true
+ }
+];
